957cf91ae6683d893fd243ca3a0733ce7e870619
Jianqiang LI <1155157143@link.cuhk.edu.hk>
2023-04-25 15:30:15 +0800

    Add files via upload

--- WTP-base/wReceiver.cpp
+++ WTP-base/wReceiver.cpp
@@ -29,14 +29,14 @@ static const int MAX_AVAILABLE_SIZE = 1472;
 static const int MAX_ACTUAL_SIZE = 1456;
 static const int IP_HEADER = 20;
 static const int UDP_HEADER = 8;
-static const int WTP_HEADER = 16; 
+static const int WTP_HEADER = 16;
 
 // clear=0 -> append new information line to log file
 // claer=1 -> clean the log file and do NOT write anything
 void out2log(string log_name, unsigned int type, unsigned int seqNum,
-            unsigned int length, unsigned int checksum, int clear=0){
+            unsigned int length, unsigned int checksum, int clear_flag=0){
     FILE *fp;
-    if(clear){
+    if(clear_flag == 1){
         fp = fopen(log_name.c_str(), "w+");
         return;
     }
@@ -47,11 +47,173 @@ void out2log(string log_name, unsigned int type, unsigned int seqNum,
 }
 
 int main(int argc, const char **argv){
+    // handle input command
     int listen_port = atoi(argv[1]);
     int window_size = atoi(argv[2]);
     string output_dir = string(argv[3]);
     string log_dir = string(argv[4]);
 
-    
+    // create a UDP socket
+    int s = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    if (s == -1)
+    {
+        perror("Fail to create socket.\n");
+        exit(1);
+    }
+
+    // create address structure for sender and receiver
+    struct sockaddr_in add_server, add_client;
+    socklen_t slen = sizeof(add_client);
+    memset((char *) &add_server, 0, sizeof(add_server));
+
+    add_server.sin_family = AF_INET;
+    add_server.sin_addr.s_addr = htonl(INADDR_ANY); // ip address
+    add_server.sin_port = htons(listen_port); // port adress
+
+    // bind socket to port
+    if(bind(s, (struct sockaddr*)&add_server, sizeof(add_server)) == -1)
+    {
+        perror("Fail to bind.\n");
+        exit(1);
+    }
+
+    // ready to receive data
+    int byte_count, file_count = 0;
+    while (1)
+    { 
+        char data_file_dir[256] = {0};
+        sprintf(data_file_dir, "%s/FILE-%d.out", output_dir, file_count);
+
+        // wait for a new file
+        char buffer[1510] = {0};
+        byte_count = recvfrom(s, buffer, 1500, 0, (struct sockaddr *) &add_client, &slen);
+        if (byte_count == -1)
+        {
+            perror("error with recvfrom().\n");
+            exit(1);
+        }
+        buffer[byte_count] = '\0';
+
+        // hanlde start header
+        PacketHeader *header = (PacketHeader*)buffer;
+        out2log(log_dir, header->type, header->seqNum, header->length, header->checksum, 0);
+
+        //generate ack header
+        header->type = 3; // now header can be used as ack header
+        char ack_buffer[1510] = {0};
+        int header_length = sizeof(*header);
+        memcpy(ack_buffer, header, sizeof(*header));
+        sendto(s, ack_buffer, sizeof(ack_buffer), 0, (struct sockaddr*) &add_client, slen);
+        out2log(log_dir, header->type, header->seqNum, header->length, header->checksum, 0);
+
+        // receive actual data
+        char data[3000][1510] = {0}; // assume seqNum < 3000, packetSize < 1510
+        int seq_length[3000] = {0};
+        int current_seq = -1, end_seq = -1;
+        unsigned int my_checksum;
+        char data_header_buffer[20] = {0};
+        while (1)
+        {
+            int packet_count = 0;
+            for(int i = 0; i < window_size; i++)
+            {
+                memset(buffer, 0, 1510);
+                byte_count = recvfrom(s, buffer, 1472, MSG_DONTWAIT, (struct sockaddr *) &add_client, &slen);
+                if (byte_count != -1)
+                {
+                    packet_count++;
+                    buffer[byte_count] = '\0';
+                    // get header info
+                    for (int j = 0; j < header_length; j++)
+                    {
+                        data_header_buffer[j] = buffer[j];
+                    }
+                    PacketHeader *data_header = (PacketHeader*)data_header_buffer;
+                    out2log(log_dir, data_header->type, data_header->seqNum, data_header->length, data_header->checksum, 0);
+
+                    // if end type, break
+                    if(data_header->type == 1)
+                    {
+                        end_seq = data_header->seqNum;
+                        break;
+                    }
+
+                    // else handle data part
+                    if(data_header->type == 2 && data_header->seqNum == current_seq + 1)
+                    {
+                        // store data to buffer
+                        for (int j = 0; j < data_header->length; j++)
+                        {
+                            data[data_header->seqNum][j] = buffer[header_length+j];
+                        }
+                        data[data_header->seqNum][data_header->length] = '\0';
+
+                        // calculate local checksum
+                        my_checksum = crc32(data[data_header->seqNum], data_header->length);
+                        // if not match, drop packet
+                        if (my_checksum != data_header->checksum)
+                        {
+                            memset(data[data_header->seqNum], 0, 1510);
+                        }
+                        //if match, update current_seq
+                        else
+                        {
+                            seq_length[data_header->seqNum] = data_header->length;
+                            current_seq = data_header->seqNum;
+                        }
+                    }
+                }
+            }
+
+            // prepare ack header
+            PacketHeader *ack_header;
+            ack_header->type = 3;
+            ack_header->length = 0;
+            memset(ack_buffer, 0, 1510);
+
+            //if end, break
+            if (end_seq != -1)
+            {
+                ack_header->seqNum = end_seq;
+                ack_header->checksum = 0;
+                memcpy(ack_buffer, ack_header, header_length);
+                sendto(s, ack_buffer, sizeof(ack_buffer), 0, (struct sockaddr*) &add_client, slen);
+                out2log(log_dir, ack_header->type, ack_header->seqNum, ack_header->length, ack_header->checksum, 0);
+                break;
+            }
+
+            //if not, ack for next packet
+            else if (packet_count > 0)
+            {
+                ack_header->seqNum = current_seq + 1;
+                ack_header->checksum = 0;
+                memcpy(ack_buffer, ack_header, header_length);
+                sendto(s, ack_buffer, sizeof(ack_buffer), 0, (struct sockaddr*) &add_client, slen);
+                out2log(log_dir, ack_header->type, ack_header->seqNum, ack_header->length, ack_header->checksum, 0);
+            }
+        }
+
+        // now store data to output file
+        ofstream output_stream;
+        output_stream.open(data_file_dir, std::ios_base::app);
+        if ( !output_stream )
+        {
+            perror("Fail to open file.\n");
+            exit(1);
+        }
+        for (int i = 0; i <= current_seq; i++)
+        {
+            output_stream.write(data[i], seq_length[i]);
+        }
+        if ( !output_stream )
+        {
+            perror("Fail to write file.\n");
+            exit(1);
+        }
+        output_stream.close();
+        file_count++;
+    }
+
+
     return 0;
 }
